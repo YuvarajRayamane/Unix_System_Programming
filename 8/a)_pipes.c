e01ef1d285d0f221a657448c40eb9f0152b275a5
Yuvaraj Rayamane <102849877+YuvarajRayamane@users.noreply.github.com>
2022-08-25 17:15:16 +0530

    Update a)_pipes.c

--- 8/a)_pipes.c
+++ 8/a)_pipes.c
@@ -1,44 +1,25 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
-#define STRING_SIZE 512
-
-int main() {
-    int pipe_fds1[2], pipe_fds2[2];
-    char read_message[STRING_SIZE];
-    char pipe1_write_message[STRING_SIZE] = "Hi";
-    char pipe2_write_message[STRING_SIZE] = "Hello";
-
-    if (pipe(pipe_fds1) == -1) {
-        printf("Unable to create pipe 1\n");
-        return 1;
-    }
-
-    if (pipe(pipe_fds2) == -1) {
-        printf("Unable to create pipe 2\n");
-        return 1;
-    }
-
-
-    if (fork() > 0) {
-        close(pipe_fds1[0]);
-        close(pipe_fds2[1]);
-
-        printf("In parent process, writing \"%s\" to pipe 1\n", pipe1_write_message);
-        write(pipe_fds1[1], pipe1_write_message, sizeof(pipe1_write_message));
-
-        read(pipe_fds2[0], read_message, sizeof(read_message));
-        printf("In parent process, reading \"%s\" from pipe 2\n", read_message);
-    } else {
-        close(pipe_fds1[1]);
-        close(pipe_fds2[0]);
-
-        read(pipe_fds1[0], read_message, sizeof(read_message));
-        printf("In child process, reading \"%s\" from pipe 1\n", read_message);
-
-        printf("In child process, writing \"%s\" to pipe 2\n", pipe2_write_message);
-        write(pipe_fds2[1], pipe2_write_message, sizeof(pipe2_write_message));
-    }
-
-    return 0;
+int main()
+{
+int fd[2];
+pipe(fd);
+int cid = fork();
+if(cid==0)
+{
+char content[100];
+int res = close(fd[0]);
+printf("I am a child and write the content which you want to enter into the file\n ");
+scanf("%[^\n]%*c",content);
+write(fd[1],content,sizeof(content)+1);
+}
+else{
+char content[100];
+int res = close(fd[1]);
+printf("I am a parent and waiting for the content which you entered into the file\n ");
+read(fd[0],content,sizeof(content));
+printf("%s",content);
+}
 }
