421c5455513944701ac4fa5ae4964b1b14f8f9db
Yuvaraj Rayamane <102849877+YuvarajRayamane@users.noreply.github.com>
2022-10-15 22:41:21 +0530

    Delete Counts_line.sh

--- Shell_Script/Counts_line.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -e "$1" ]; then  #input of file given as command. Ex: ./2a.sh file_name
-  echo "File $1 does not exist"
-  exit
-fi
-
-line_count=$(grep "" -c "$1")
-echo "There are $line_count lines in $1"
